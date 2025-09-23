b4f926346e3ffbd054660dde2f3ef1626c02b68c
ahwar <20405797+Ahwar@users.noreply.github.com>
2025-09-23 17:50:36 +0500

    fix: face detection inference running.
    with new openvino architecture and inference api changes
    now we are running only face detection inference.
    you can run the face detection from `python lib/face_detector.py`

--- lib/face_detector.py
+++ lib/face_detector.py
@@ -1,32 +1,27 @@
-import os
-import sys
 import cv2
 import numpy as np
-import logging as log
-from openvino.inference_engine import IECore
-'''
+import openvino as ov
+
+"""
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
-'''
+"""
 
 
 class FaceDetector:
-    '''
+    """
     Class for the Face Detection Model.
-    '''
-    def __init__(self, model_name, device='CPU', extensions=None):
-        ''' Use this to set your instance variables. '''
+    """
+
+    def __init__(self, model_name, device="CPU"):
+        """Use this to set your instance variables."""
         self.core = None
-        self.network = None
-        self.input_blob = None
-        self.output_blob = None
-        self.exec_network = None
+        self.compiled_model = None
         self.infer_request = None
         self.device = device
-        self.extenstions = extensions
         self.model_xml = model_name
 
     def load_model(self):
-        '''
+        """
         Load Model file and create Executable Network
 
         This method is for loading the model to the device specified by the user.
@@ -34,96 +29,64 @@ class FaceDetector:
         Initialize Core
         Read model in to IENetwork and add any necessary Extension
         Check for Supported Layers
-        '''
-        self.model_weights = os.path.splitext(self.model_xml)[0] + ".bin"
+        """
         ### Load the model ###
         # Take model .xml and .bin file and create IENetwork
-        self.core = IECore()
-        self.network = self.core.read_network(model=self.model_xml,
-                                              weights=self.model_weights)
+        self.core = ov.Core()
+        self.compiled_model = self.core.compile_model(self.model_xml, "AUTO")
 
-        ### Add any necessary extensions ###
-        try:
-            if self.extenstions and self.device == 'CPU':
-                self.core.add_extension(extension_path=self.extenstions,
-                                        device_name=self.device)
-        except:
-            "Error in Loading {} Extension".format(self.device)
-
-        ### Check model for unsupported layers
-        self.check_model()
-
-        # retrieve name of model's input layer
-        self.input_blob = next(iter(self.network.input_info))
-
-        # retrieve name of model's output layer
-        self.output_blob = next(iter(self.network.outputs))
-
-        ### load IENetwork to Executable Network ###
-        ### Note: You may need to update the function parameters. ###
-        self.exec_network = self.core.load_network(network=self.network,
-                                                   device_name=self.device,
-                                                   num_requests=1)
+        self.infer_request = self.compiled_model.create_infer_request()
 
     def predict(self, image):
-        '''
+        """
         Perform Inference on Image and return Raw Output.
 
         This method is meant for running predictions on the input image.
         Parameters:
             image (numpy.ndarray): Frame from input file
-        
+
         Returns:
             model_output (numpy.ndarray): Raw Model Output
-        '''
+        """
+
         ### PreProcess input image according to model Requirement
         input_img = self.preprocess_input(image)
+        input_tensor = ov.Tensor(input_img, shared_memory=False)
         ### run inference and return output
         # Start Async Inference Request
-        infer_request_handle = self.exec_network.start_async(
-            request_id=0, inputs={self.input_blob: input_img})
+        self.infer_request.set_input_tensor(input_tensor)
+        self.infer_request.start_async()
+        self.infer_request.wait()
 
-        # wait for the output and return.
-        if infer_request_handle.wait(-1) == 0:
-            model_output = infer_request_handle.outputs[self.output_blob]
+        model_output = self.infer_request.get_output_tensor().data
+
         return model_output
 
-    def check_model(self):
-        """Check for supported layers"""
-        layers_map = self.core.query_network(network=self.network,
-                                             device_name=self.device)
-
-        unsupported_layers = [
-            l for l in self.network.layers.keys() if l not in layers_map
-        ]
-
-        if (unsupported_layers != []):
-            sys.exit("Those mention layers in your model are not supported by OpenVino Inference Engine:" \
-                     " \n\t" + "\n\t".join(unsupported_layers))
-
     def preprocess_input(self, image):
-        '''
+        """
         Before feeding the data into the model for inference,
         you might have to preprocess it. This function is where you can do that.
-        '''
+        """
         (b, c, h, w) = self.get_input_shape()
         image = cv2.resize(image, (w, h))
         image = np.transpose(image, (2, 0, 1))
         image = image.reshape(b, c, h, w)
+        image = image.astype(np.float32)
+        image = image / 255.0
 
         return image
 
-    def preprocess_output(self, outputs, threshold, image, image_w, image_h):
-        '''
+    def postprocess_output(self, outputs, threshold, image, image_w, image_h):
+        """
         Before feeding the output of this model to the next model,
         you might have to preprocess the output. This function is where you can do that.
-        '''
+        """
         for out in outputs[0][0]:
             conf = out[2]
             maxi = 0
             det = None
             cropped_face = None
-            if (conf > threshold and conf > maxi):
+            if conf > threshold and conf > maxi:
                 det = out
                 maxi = conf
             if det is not None:
@@ -133,5 +96,15 @@ class FaceDetector:
             return cropped_face, ((x_min, y_min), (x_max, y_max))
 
     def get_input_shape(self):
-        """ Return the shape of the input layer """
-        return self.network.inputs[self.input_blob].shape
+        """Return the shape of the input layer"""
+        return self.compiled_model.inputs[0].shape
+
+
+if __name__ == "__main__":
+    detector = FaceDetector(r"bin\models\1\face-detection-retail-0004.xml")
+    detector.load_model()
+    image = cv2.imread("bin/face.png")
+    print("Model Input shape:", detector.get_input_shape())
+    print("image size", image.shape)
+    output = detector.predict(image)
+    print("Model Output:", output.shape)
